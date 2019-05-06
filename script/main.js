20ac5042c443bd2536f3471568fa626a9aaaf438
cieslaluk <ciesla.luk@gmail.com>
2019-05-06 15:05:04 +0200

    added_bg_image_major_updates

--- script/main.js
+++ script/main.js
@@ -1,7 +1,26 @@
 const CARDS = document.querySelectorAll('.memory_card');
+let hasFlippedCard = false;
+let firstCard, secondCard;
 
 function flipCard(){
 this.classList.toggle('flip');
+
+if(!hasFlippedCard){
+    hasFlippedCard = true;
+    firstCard = this;
+} else {
+    hasFlippedCard = false;
+    secondCard = this;
+
+    if (firstCard.dataset.name === secondCard.dataset.name){
+        firstCard.removeEventListener('click', flipCard);
+        secondCard.removeEventListener('click', flipCard)
+    } else {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+    }
+}
 };
+
 
 CARDS.forEach(card => card.addEventListener('click', flipCard))
