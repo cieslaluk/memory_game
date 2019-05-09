bcf0f6b777dcf61458741003d95852f0798231fc
cieslaluk <ciesla.luk@gmail.com>
2019-05-09 11:33:04 +0200

    added_shuffle_logic

--- script/main.js
+++ script/main.js
@@ -1,26 +1,48 @@
 const CARDS = document.querySelectorAll('.memory_card');
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard(){
+    if(lockBoard) return;
+    if(this === firstCard) false;
+
 this.classList.toggle('flip');
 
 if(!hasFlippedCard){
     hasFlippedCard = true;
     firstCard = this;
 } else {
-    hasFlippedCard = false;
     secondCard = this;
 
     if (firstCard.dataset.name === secondCard.dataset.name){
         firstCard.removeEventListener('click', flipCard);
         secondCard.removeEventListener('click', flipCard)
+        resetBoard();
     } else {
-        firstCard.classList.remove('flip');
-        secondCard.classList.remove('flip');
+        lockBoard = true;
+        setTimeout(()=>{
+            firstCard.classList.remove('flip');
+            secondCard.classList.remove('flip');
+            
+            resetBoard();
+        }, 1500);
+       
     }
 }
 };
+
+function resetBoard(){
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle(){
+    CARDS.forEach(card =>{
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    });
+})();
 
 
 CARDS.forEach(card => card.addEventListener('click', flipCard))
